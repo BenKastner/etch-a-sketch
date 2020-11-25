e475c1ffcceda0ec6e9a644b4bad96181cb52456
BenKastner <ben.kastner@gmail.com>
2020-11-24 18:03:33 -0600

    Now createGrid function works

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const container = document.querySelector(".container");
 
 function createGrid(rows, cols) {
-    container.style.setProperty('--grid-rows, rows');
-    container.style.setProperty('--grid-cols, cols');
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
     };
 };
-
-makeRows(16, 16);
