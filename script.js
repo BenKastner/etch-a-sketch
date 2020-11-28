d6cd4de6fd90d860ecbb5a297bdc0fe7cde6dc4c
BenKastner <ben.kastner@gmail.com>
2020-11-28 00:15:30 -0600

    Add removeGrid function, clear button, prompt for new grid

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 const container = document.querySelector(".container");
+const clearButton = document.querySelector(".clear");
+let num = 16;
 
-function createGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+function createGrid(num) {
+    container.style.setProperty('--grid-rows', num);
+    container.style.setProperty('--grid-cols', num);
+    for (c = 0; c < (num * num); c++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
     };
+
+    container.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = randomHsl();
+    });
 };
 
-function randomHSL() {
-    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1';
+/* function removeGrid(num) {
+    for (c = 0; c < (num * num); c++) {
+        let square = document.querySelector(".grid-item");
+        container.removeChild(square);
+    };
+} */
+
+createGrid(num);
+
+function clearGrid () {
+    for (c = 0; c < (num * num); c++) {
+        let square = document.querySelector(".grid-item");
+        container.removeChild(square);
+    }
+    num = prompt("How many squares on each side of new grid?");
+    createGrid(num);
 }
 
-container.addEventListener("mouseover", function( event ) {
-    event.target.style.backgroundColor = randomHSL();
-});
+function randomHsl() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
 
-createGrid(20, 20);
+const gridItem = document.querySelectorAll(".grid-item");
+
+/* gridItem.forEach((div) => {
+
+    div.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = randomHsl();
+    });
+
+}); */
+
+clearButton.addEventListener("click", clearGrid);
